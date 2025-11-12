2574aaab5d73a4e4a2610c824c5ee164ab12c33f
Gabriel Vivas <gabovivas2002@icloud.com>
2025-11-12 19:21:23 +0100

    se creó el archivo utils_redir.c para la gestión de la estructura redir.

---
+++ src/utils_redir.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils_redir.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gavivas- <gavivas-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/12 19:12:32 by gavivas-          #+#    #+#             */
+/*   Updated: 2025/11/12 19:20:40 by gavivas-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <minishell.h>
+
+t_redir	*redir_new(t_rtype type, char *file)
+{
+}
+
+t_redir	*redir_last(t_redir *lst)
+{
+}
+
+void	redir_add(t_redir **lst, t_redir *new)
+{
+}
+
+void	redir_clear(t_redir **lst)
+{
+}
